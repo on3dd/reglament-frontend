6c5ef290d70d4b78a3c8fbf774feda14b5fb5f46
on3dd <onedeadwave@gmail.com>
2020-12-21 14:54:47 +1000

    table 2/2

--- src/components/table/body/index.tsx
+++ src/components/table/body/index.tsx
@@ -4,52 +4,18 @@ import styled from 'styled-components';
 
 import { columns, data } from './config';
 
-const BodyContainer = styled.div``;
+import TableBodyTable from './table';
 
-const TableBodyTable = styled.table`
-  width: 100%;
+const BodyContainer = styled.div`
+  margin-bottom: 0.85rem;
 `;
 
 const TableBody: React.FC = () => {
-  const tableInstance = useTable({ columns, data });
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = tableInstance;
+  const instance = useTable({ columns, data });
 
   return (
     <BodyContainer className="table__body">
-      <TableBodyTable {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>
-                  {column.render('Header')}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row) => {
-            prepareRow(row);
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => {
-                  return (
-                    <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
-      </TableBodyTable>
+      <TableBodyTable instance={instance} />
     </BodyContainer>
   );
 };
---
+++ src/components/table/body/table/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { TableInstance } from 'react-table';
+import styled, { css } from 'styled-components';
+
+import { Document } from '@reglament';
+
+type TableBodyTableProps = {
+  instance: TableInstance<Document>;
+};
+
+const border = css`
+  border: 1px solid #acacac;
+`;
+
+const cell = css`
+  padding: 0.5rem;
+  border-top: none;
+  text-align: center;
+`;
+
+const Table = styled.table`
+  ${border}
+
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const Th = styled.th`
+  ${border}
+  ${cell}
+
+  color: #0066B3;
+`;
+
+const Td = styled.td`
+  ${border}
+  ${cell}
+
+  border-bottom: none;
+`;
+
+const TableBodyTable: React.FC<TableBodyTableProps> = ({
+  instance,
+}: TableBodyTableProps) => {
+  const {
+    rows,
+    headerGroups,
+    prepareRow,
+    getTableProps,
+    getTableBodyProps,
+  } = instance;
+
+  return (
+    <Table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <Th {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </Th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
+
+export default TableBodyTable;

94e4e4987b15515be4e4e98fc0f5b50af1c42998
on3dd <onedeadwave@gmail.com>
2020-12-28 03:52:55 +1000

    table link to admin

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react';
 import { autorun } from 'mobx';
 import { observer } from 'mobx-react-lite';
+import { Link } from 'react-router-dom';
 import { useTable, usePagination } from 'react-table';
 import styled from 'styled-components';
 
+import { PropsWithTheme } from '@reglament';
+
+import { useAuthStore } from '../../store/auth';
 import { useSearchStore } from '../../store/search';
 
+import { useThemeContext } from '../../utils/contexts/ThemeContext';
+
 import { columns, initialState } from './config';
+
+import { pointer, lowercase } from '../base-ui/mixins';
 
 import TableHeader from './header';
 import TableBody from './body';
@@ -14,8 +22,41 @@ import TableFooter from './footer';
 
 const TableContainer = styled.div``;
 
+type TableLinkToAdminProps = PropsWithTheme<{}>;
+
+const TableLinkToAdmin = styled(Link)`
+  ${pointer}
+  ${lowercase}
+
+  display: block;
+  position: fixed;
+  right: 0;
+  bottom: 6.75rem;
+  font-size: 1.25rem;
+  text-decoration: none;
+  padding: 0.5rem 1rem 0.5rem 2rem;
+  border-radius: 2.5rem 0px 0px 2.5rem;
+
+  color: ${({ theme }: TableLinkToAdminProps) => {
+    return theme.fontColorNeg;
+  }};
+
+  background-color: ${({ theme }: TableLinkToAdminProps) => {
+    return theme.buttonColor;
+  }};
+
+  &:hover,
+  &:focus {
+    text-decoration: underline;
+  }
+`;
+
 const TableComponent: React.FC = observer(() => {
+  const {
+    store: { token },
+  } = useAuthStore();
   const { store } = useSearchStore();
+  const { theme } = useThemeContext();
 
   const instance = useTable(
     {
@@ -37,6 +78,12 @@ const TableComponent: React.FC = observer(() => {
       <TableHeader />
       <TableBody instance={instance} />
       <TableFooter instance={instance} />
+
+      {token && (
+        <TableLinkToAdmin theme={theme} to="/admin">
+          Администрирование
+        </TableLinkToAdmin>
+      )}
     </TableContainer>
   );
 });

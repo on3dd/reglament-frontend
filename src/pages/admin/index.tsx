a124443dbeb0d65468d613e28a0d482a246387ff
on3dd <onedeadwave@gmail.com>
2020-12-28 01:44:53 +1000

    admin route protection

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Redirect } from 'react-router';
 import {
   renderRoutes,
   RouteConfigComponentProps,
 } from 'react-router-config';
+import { observer } from 'mobx-react-lite';
 import styled from 'styled-components';
+
+import { useAuthStore } from '../../store/auth';
 
 type AdminProps = RouteConfigComponentProps<{}>;
 
 const Container = styled.div``;
 
-const Admin: React.FC<AdminProps> = ({ route }: AdminProps) => {
-  return <Container>{route && renderRoutes(route.routes)}</Container>;
-};
+const Admin: React.FC<AdminProps> = observer(({ route }: AdminProps) => {
+  const { store } = useAuthStore();
+
+  const renderRouter = useCallback(() => {
+    return route ? renderRoutes(route.routes) : null;
+  }, [route]);
+
+  const renderRedirect = useCallback(() => {
+    return <Redirect to="/auth" />;
+  }, []);
+
+  return (
+    <Container>
+      {store.token ? renderRouter() : renderRedirect()}
+    </Container>
+  );
+});
 
 export default Admin;
